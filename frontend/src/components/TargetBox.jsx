39b4ee21199e2f7306f4eb746629b2856659e949
jurazick <j.suxrobjon55@gmail.com>
2026-06-15 17:12:03 -0400

    coordinates and targetbox options

--- frontend/src/components/TargetBox.jsx
+++ frontend/src/components/TargetBox.jsx
@@ -1,9 +1,9 @@
 export default function TargetBox({x = 0,y = 0}) {
     return(<>
-        <div className={`bg-zinc-800 text-gray-300`}>
-            <p>Target Box</p>
-            <p>X: {x}</p>
-            <p>Y: {y}</p>
+        <div className={` text-gray-300 w-20 flex flex-col justify-center gap-2`}>
+            <button className="bg-zinc-800 p-1 cursor-pointer">Waldo</button>
+            <button className="bg-zinc-800 p-1 cursor-pointer">Wizard</button>
+            <button className="bg-zinc-800 p-1 cursor-pointer">Odlaw</button>
         </div>
     </>)
 }
--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -3,13 +3,19 @@ import wheresWaldoBeach from "../assets/wheres-waldo-beach.jpeg"
 import TargetBox from "../components/TargetBox"
 
 export default function Game() {
-    const [box, setBox] = useState({x: 0, y: 0, show: false})
+    const [box, setBox] = useState({x: 0, y: 0, top: 0, left: 0, show: false})
 
 
 
     function handleClick(e) {
-        console.log(`(${e.clientX}, ${e.clientY})`)
-        setBox(prev => ({x: e.clientX, y: e.clientY, show: !prev.show}))
+        const rect = e.target.getBoundingClientRect();
+        const xImage = e.clientX - rect.left
+        const yImage = e.clientY - rect.top
+
+        const x = (xImage * 100 / rect.width).toFixed(0)
+        const y = (yImage * 100 / rect.height).toFixed(0)
+        console.log(`(${x}, ${y})`)
+        setBox(prev => ({x, y, top: e.clientY, left: e.clientX, show: !prev.show}))
     }
 
     return(
@@ -18,7 +24,7 @@ export default function Game() {
             <div className="bg-amber-400 p-5">
                 <img className="cursor-crosshair" onClick={handleClick} src={wheresWaldoBeach}></img>
                 {box.show && (
-                    <div className={`absolute`} style={{top: box.y-100, left: box.x-50}}>
+                    <div className={`absolute`} style={{top: box.top+20, left: box.left-40}}>
                         <TargetBox x={box.x} y={box.y} />
                     </div>
                 )}
