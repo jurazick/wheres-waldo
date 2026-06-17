c444c4cbc16d43cc0c42ee28b1b0e856312514ac
jurazick <j.suxrobjon55@gmail.com>
2026-06-17 11:13:33 -0400

    target box component & coords

--- frontend/src/components/TargetBox.jsx
+++ frontend/src/components/TargetBox.jsx
@@ -1,9 +1,13 @@
-export default function TargetBox({x = 0,y = 0}) {
+export default function TargetBox({handleSubmit = () => {}, characters = []}) {
+
+    console.log(characters)
+
     return(<>
-        <div className={` text-gray-300 w-20 flex flex-col justify-center gap-2`}>
-            <button className="bg-zinc-800 p-1 cursor-pointer">Waldo</button>
-            <button className="bg-zinc-800 p-1 cursor-pointer">Wizard</button>
-            <button className="bg-zinc-800 p-1 cursor-pointer">Odlaw</button>
+        <div className={` text-gray-300 text-xs w-20 flex flex-col items-center gap-2`}>
+            <div className="h-6 w-6 border-green-600 border-dashed opacity-75 border-2"></div>
+            {characters.map((character) => 
+                <button key={character} className="bg-zinc-800 p-1 cursor-pointer w-15" onClick={() => handleSubmit(character)}>{character}</button>
+            )}
         </div>
     </>)
 }
--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -2,10 +2,16 @@ import { useState } from "react"
 import wheresWaldoBeach from "../assets/wheres-waldo-beach.jpeg"
 import TargetBox from "../components/TargetBox"
 
+const COORDS = {
+    "Waldo": [62, 37],
+    "Wizard": [27,35],
+    "Odlaw": [11, 35]
+}
+
+
+
 export default function Game() {
     const [box, setBox] = useState({x: 0, y: 0, top: 0, left: 0, show: false})
-
-
 
     function handleClick(e) {
         const rect = e.target.getBoundingClientRect();
@@ -18,14 +24,21 @@ export default function Game() {
         setBox(prev => ({x, y, top: e.clientY, left: e.clientX, show: !prev.show}))
     }
 
+    function handleSubmit(character) {
+        setBox(prev => ({...prev, show: false}))
+        if (Math.abs(box.x - COORDS[character][0]) <= 2 && Math.abs(box.y - COORDS[character][1]) <= 3) {
+            console.log(`FOUND ${character}`)
+        }
+    }
+
     return(
         <>
             <h1>Find Waldo</h1>
             <div className="bg-amber-400 p-5">
                 <img className="cursor-crosshair" onClick={handleClick} src={wheresWaldoBeach}></img>
                 {box.show && (
-                    <div className={`absolute`} style={{top: box.top+20, left: box.left-40}}>
-                        <TargetBox x={box.x} y={box.y} />
+                    <div className={`absolute`} style={{top: box.top-10, left: box.left-40}}>
+                        <TargetBox handleSubmit={handleSubmit} characters={Object.keys(COORDS)} />
                     </div>
                 )}
             </div>
